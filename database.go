019dfaebefab788d3850f8d8571a08c36ff40bb0
Jakub Kaczmarzyk <jakubk@mit.edu>
2020-01-06 10:02:34 -0500

    move `PatientList` methods to `database.go`

--- database.go
+++ database.go
@@ -1,3 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 var Patients = make(PatientList, 0)
+
+type PatientList []*Patient
+
+func AddPatientToList(p *Patient) {
+	Patients = append(Patients, p)
+}
+
+func (p *PatientList) Get(id int) (*Patient, error) {
+	if id < 0 || id >= len(Patients) {
+		return nil, fmt.Errorf("patient %d not found", id)
+	}
+	return Patients[id], nil
+}
+
+func (p *PatientList) Equals(q *PatientList) bool {
+	if (p != nil && q == nil) || (p == nil && q != nil) {
+		return false
+	}
+	if len(*p) != len(*q) {
+		return false
+	}
+	for i := range *p {
+		if !reflect.DeepEqual((*p)[i], (*q)[i]) {
+			return false
+		}
+	}
+	return true
+}
--- models.go
+++ models.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"reflect"
 )
 
 type Patient struct {
@@ -26,34 +25,6 @@ func (p *Patient) ModifyDOB(dob string) {
 	} else {
 		p.DOB = dob
 	}
-}
-
-type PatientList []*Patient
-
-func AddPatientToList(p *Patient) {
-	Patients = append(Patients, p)
-}
-
-func (p *PatientList) Get(id int) (*Patient, error) {
-	if id < 0 || id >= len(Patients) {
-		return nil, fmt.Errorf("patient %d not found", id)
-	}
-	return Patients[id], nil
-}
-
-func (p *PatientList) Equals(q *PatientList) bool {
-	if (p != nil && q == nil) || (p == nil && q != nil) {
-		return false
-	}
-	if len(*p) != len(*q) {
-		return false
-	}
-	for i := range *p {
-		if !reflect.DeepEqual((*p)[i], (*q)[i]) {
-			return false
-		}
-	}
-	return true
 }
 
 func NewPatient(name, dob string) *Patient {
